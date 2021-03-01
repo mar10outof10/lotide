71b58bac8e4007edb3b0f5b656a64ea836461efa
Martin Halas <echoes.time@gmail.com>
2021-03-01 23:01:03 +0000

    Adding assertion test for arrays"

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actualArray, expectedArray) {
+  if (actualArray.length !== expectedArray.length) {
+    return false;
+  }
+  for (let i = 0; i < actualArray.length; i++) {
+    if (actualArray[i] !== expectedArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
