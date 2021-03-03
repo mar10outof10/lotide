75d277456796e0d98700d7286b5357ba17bba0ee
Martin Halas <echoes.time@gmail.com>
2021-03-03 22:24:42 +0000

    basic map function with assertion tests for lotide library

---
+++ map.js
@@ -0,0 +1,37 @@
+const words = [ "to", "slay", "all", "the", "giants"];
+
+const eqArrays = function(actualArray, expectedArray) {
+  if (actualArray.length !== expectedArray.length) {
+    return false;
+  }
+  for (let i = 0; i < actualArray.length; i++) {
+    
+    if (typeof(actualArray[i]) === 'object' && eqArrays(actualArray[i], expectedArray[i])) {
+      continue;
+    }
+    if (actualArray[i] !== expectedArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, ["t", "s", "a", "t", "g"]);
