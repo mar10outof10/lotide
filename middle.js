6b0f1295298fd843170ab21b90acab1051421b2e
Martin Halas <echoes.time@gmail.com>
2021-03-08 19:46:55 +0000

    refactored middle to use mocha

--- middle.js
+++ middle.js
@@ -1,46 +1,15 @@
-const eqArrays = function(actualArray, expectedArray) {
-  if (actualArray.length !== expectedArray.length) {
-    return false;
-  }
-  for (let i = 0; i < actualArray.length; i++) {
-    
-    if (typeof(actualArray[i]) === 'object' && eqArrays(actualArray[i], expectedArray[i])) {
-      continue;
-    }
-    if (actualArray[i] !== expectedArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const middle = function(array) {
-  if (array.length < 3) {
+const middle = array => {
+  const length = array.length;
+  if (length < 3) {
     return [];
   }
   
-  if (array.length % 2 === 0) {
-    return array.slice(array.length / 2 - 1, array.length / 2 + 1);
+  if (length % 2 === 0) {
+    return array.slice(length / 2 - 1, length / 2 + 1);
   } else {
-    return array.slice(Math.floor(array.length / 2), Math.ceil(array.length / 2));
+    return array.slice(Math.floor(length / 2), Math.ceil(length / 2));
   }
 
 };
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])); // true
-console.log(assertArraysEqual(middle([]), [])); // true
-console.log(assertArraysEqual(middle([1]), [])); // true
-console.log(assertArraysEqual(middle([1, 2, 3]), [2])); // true
-console.log(assertArraysEqual(middle([1, [2, 2], 3]), [[2, 2]])); // true
-console.log(assertArraysEqual(middle([1, [2, 2], 3]), [2, 2])); // false (should be array within array)
-console.log(assertArraysEqual(middle([1, [2, 3], 4, 5]), [[2, 3], 4])); // true
-console.log(assertArraysEqual(middle([1, 2, [3, 4, 5]]), [2])); // true
-
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,21 @@
+const { assert } = require('chai');
+const middle = require('../middle');
+
+
+describe('#middle', () => {
+  it("Should return the middle 2 elements of an even length array", () =>{
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("Should return the single middle element of an odd length array", () =>{
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("Should return an empty array if the array length is less than 3", () =>{
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("Should return a properly nested array if the middle elements are nested", () =>{
+    assert.deepEqual(middle([1, [2, 2], 3]), [[2, 2]]);
+  });
+  it("Should return a properly nested array if the middle elements are nested", () =>{
+    assert.notDeepEqual(middle([1, [2, 2], 3]), [2, 2]); //expected should be nested
+  });
+});
