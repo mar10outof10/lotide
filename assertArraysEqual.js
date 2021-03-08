d2a0103b71a48613eabf70c76a95b60e334b7875
Martin Halas <echoes.time@gmail.com>
2021-03-08 19:36:04 +0000

    Refactoring code to import eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-const eqArrays = function(actualArray, expectedArray) {
-  if (actualArray.length !== expectedArray.length) {
-    return false;
-  }
-  for (let i = 0; i < actualArray.length; i++) {
-    
-    if (typeof(actualArray[i]) === 'object' && eqArrays(actualArray[i], expectedArray[i])) {
-      continue;
-    }
-    if (actualArray[i] !== expectedArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -21,14 +7,3 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-// assertArraysEqual(["1", ["3", "2"]], ["1", ["3", "2"]]); // => true
-// assertArraysEqual(["1", ["3", "2"]], ["1", ["2", "3"]]); // => false
-// assertArraysEqual(["1", [1]], ["1", []]); // => false
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-
-// assertArraysEqual('aslkas', 'aslkas'); //=> true
-// assertArraysEqual('asdf', 'asfd'); // => false
